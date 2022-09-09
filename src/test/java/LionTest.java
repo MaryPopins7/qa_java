c7d6f69118c3c2ae7c5654c0115892ef49eaf2ff
MaryPopins77 <tropicana7@yandex.ru>
2022-09-09 12:06:15 +0100

    add jacoco report

---
+++ src/test/java/LionTest.java
@@ -0,0 +1,30 @@
+import com.example.Feline;
+import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+    private final String defaultSex = "Самец";
+    @Mock
+        private Feline feline;
+
+    @Test
+    public void felineGetKittensDefaultNumberTest() throws Exception {
+        Lion lion = new Lion(defaultSex, feline);
+        Assert.assertEquals(1, lion.getKittens());
+    }
+
+     @Test
+    public void lionGetFood() throws Exception {
+        Lion lionAnimal = new Lion(defaultSex, feline);
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Assert.assertEquals(expectedFood, lionAnimal.getFood());
+    }
+}
+
